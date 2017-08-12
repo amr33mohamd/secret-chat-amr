f0d90936ded28912038d87e2937d29b9dac38cab
amr33mohamd <amr2010mohamd2010@gmail.com>
2017-08-12 20:33:10 +0200

    start

---
+++ app.js
@@ -0,0 +1,27 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+const port = process.env.PORT || 5000;
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/home.html');
+  
+});
+ 
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect',function(){
+  	console.log('disconnected');
+  });
+  socket.on('message',function(msg){
+  	 io.emit('message', msg);
+  })
+});
+
+http.listen(port, function(){
+  console.log('listening on *:5000');
+});
+ 
+/*
+New: aacaa 
+previous : abcdae
+  */  
